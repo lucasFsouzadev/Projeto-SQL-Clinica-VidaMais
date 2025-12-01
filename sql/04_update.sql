67fee69e99cdc93d008f27025d5331fede157006
Lᴜᴄᴀs Sᴏᴜᴢᴀ😎 <lucaas.braziil@gmail.com>
2025-12-01 17:07:59 -0300

    Update patient, doctor, and consultation records
    # Atualizações

---
+++ sql/04_update.sql
@@ -0,0 +1,5 @@
+UPDATE Paciente SET telefone = '11999998888' WHERE paciente_id = 1;
+
+UPDATE Medico SET email = 'dr.joao@vidamais.com' WHERE medico_id = 1;
+
+UPDATE Consulta SET status = 'Cancelada' WHERE consulta_id = 2;
